f754bdc57c3879441714e64b35e968f29920b433
Guillaume MOHR <guillaume.mohr@gmail.com>
2016-06-25 13:08:14 +0200

    added pruned tree support

--- final/static/js/compute_data.js
+++ final/static/js/compute_data.js
@@ -41,15 +41,51 @@ data = d3.csv('static/data/data.csv')
 var test_new_data_0 = {country_id: "DEU", category: null};
 var test_new_data_1 = {country_id: null, category: ["root", "Sugary sncacks"]};
 
-function rename_nested(data) {
+function rename_nested_old(data) {
+	// TODO : REMOVE
 	if (data.values.constructor === Array )
 		return {name: data.key, level: "1", children: data.values.map(rename_nested)}; 
 	else return {
 		name: data.key, 
 		level: "2", 
-		children: {name: "#products = " + data.values, level:"3"
-		}
+		children: {name: "#products = " + data.values, level:"3"}
 	};
+}
+
+function rename_nested(data, level) {
+	var children = [];
+	if (data.values.constructor === Array ) 
+		children = data.values.map(function(d) {rename_nested(d, level+1)});
+	return {
+		name: data.key, 
+		level: level, 
+		children: children
+	};
+}
+
+function prune(nested_data, level, category) {
+	/*
+	 * Prune the nested data: only one element up until "level", then all of its children
+	 */
+	var children;
+	// if we are at the children we want to display
+	if (nested_data.level > level) children = [];
+	// if we are at the node clicked on by the user
+	else if (nested_data.level === level && nested_data.category === category) { 
+		children = nested_data.children.map(function(d) {prune(d, level, category);});
+	}
+	// if we are at the brother nodes that we do not want to display
+	else if (nested_data.level === level) return null;
+	// if we are at a (grand)parent or (grand)uncle nodes
+	else {
+		children = nested_data.children
+			.map(function(d) {prune(d, level, category);})
+			.filter(function(d) {d != null});
+		// if all children are null, then we should not display the node
+		if (children.length === 0) return null;
+	}
+	nested_data.children = children;
+	return nested_data;
 }
 
 function compute_data(new_data) {
@@ -71,17 +107,22 @@ function compute_data(new_data) {
 		// we compute the tree
 		nested_data = {
 			name: "root",
-			level: "0",
+			level: 0,
 			children: d3.nest()
-			.key(function(d) { return d.category; })
-			.key(function(d) { return d.subcategory; })
-			.rollup(function(l) { return l.length; })
-			//.key(function(d) { return d.product_name; })
-			//.rollup(function(l) { return null; })
-			.entries(useful_data)
-			.map(rename_nested)
-		}
-		console.log(nested_data);
+				.key(function(d) { return d.category; })
+				//.key(function(d) { return d.subcategory; })
+				//.rollup(function(l) { return l.length; })
+				//.key(function(d) { return d.product_name; })
+				.rollup(function(l) { return null; })
+				.entries(useful_data)
+				.map(function(d) {
+					return {
+						name: d.key,
+						level: 1,
+						children: []
+					};
+				})
+		};
 		// we compute the stats
 		stats = nutritionals.map(function(n) {
 			return {name: n,
@@ -91,10 +132,10 @@ function compute_data(new_data) {
 		country_id_list = [new_data.country_id];
 		query = new_data.country_id;
 	}
-	// Second case : a category or a product has been selected
+	// First case next step : a category or a product has been selected
 	else if ("category" in new_data && new_data.category != null &&
-		 "level" in new_data && level != null &
-		 "query" in new_data && level != null) {
+		 "level" in new_data && new_data.level != null &
+		 "query" in new_data && new_data.query != null) {
 		console.log("manage category !");
 		// we filter on the selected country
 		var useful_data = csv_data.filter(function(d) {
@@ -103,16 +144,18 @@ function compute_data(new_data) {
 		// we compute the tree
 		nested_data = {
 			name: "root",
+			level: 0,
 			children: d3.nest()
-			.key(function(d) { return d.category; })
-			.key(function(d) { return d.subcategory; })
-			.rollup(function(l) { return l.length; })
-			//.key(function(d) { return d.product_name; })
-			//.rollup(function(l) { return null; })
-			.entries(useful_data)
-			.map(rename_nested)
-		}
-		console.log(nested_data);
+				.key(function(d) { return d.category; })
+				.key(function(d) { return d.subcategory; })
+				.key(function(d) { return d.product_name; })
+				.rollup(function(l) { return null; })
+				.entries(useful_data)
+				.map(function(d) {rename_nested(d, 1);})
+		};
+		// we prune the tree
+		nested_data = prune(nested_data, new_data.level, new_data.category)
+		
 		// we compute the stats
 		stats = nutritionals.map(function(n) {
 			return {name: n,
