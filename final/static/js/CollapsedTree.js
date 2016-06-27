4952d6117eab7336a4aa804468bacdfcf498d41a
khalil.chourou <chouroukhalil@gmail.com>
2016-06-27 16:20:02 +0200

    fancy transitions

--- final/static/js/CollapsedTree.js
+++ final/static/js/CollapsedTree.js
@@ -25,7 +25,7 @@ var svg = d3.select("#collapsedTree").append("svg")
 // tree init
 d3.json("static/data/data.json", function(error, flare) {
   if (error) throw error;
-  console.log(flare);
+  //console.log(flare);
   root = flare;
   root.x0 = height / 2;
   root.y0 = 0;
@@ -41,6 +41,7 @@ d3.json("static/data/data.json", function(error, flare) {
 });
 // tree update
 function update_tree(dataTree,query) {
+  console.log("the returned tree");
   console.log(dataTree);
   selected_country = query;
   root = dataTree;
@@ -65,18 +66,21 @@ d3.select(self.frameElement).style("height", "800px");
 
 function update(source) {
 
-  console.log(source);
+  //console.log(source);
   // Compute the new tree layout.
   var nodes = tree.nodes(root).reverse(),
       links = tree.links(nodes);
-// tree.nodes(root.children[0]).reverse()
+  console.log("the nodes are");
   console.log(nodes);
+// tree.nodes(root.children[0]).reverse()
+  //console.log(nodes);
   // Normalize for fixed-depth.
-  nodes.forEach(function(d) { d.y = d.depth * 90; });  // changer la longueur de l'arc
+  nodes.forEach(function(d) { d.y = d.depth * 150; });  // changer la longueur de l'arc
 
   // Update the nodes…
+
   var node = svg.selectAll("g.node")
-      .data(nodes, function(d) { return d.id || (d.id = ++i); });
+      .data(nodes, function(d) { return d.id = ++i; });
 
   // Enter any new nodes at the parent's previous position.
   var nodeEnter = node.enter().append("g")
@@ -158,28 +162,68 @@ function update(source) {
 // Toggle children on click.
 
 function simulate_clicks(source) {
-  console.log(source)
+  //console.log(source)
   if (source.children) {
     source._children = source.children;
-    source._children.forEach(simulate_clicks);
+    //source._children.forEach(simulate_clicks);
     source.children = null;
     
   } else {
     source.children = source._children;
-    source.children.forEach(simulate_clicks);
+    //source.children.forEach(simulate_clicks);
     source._children = null;
     
-  }
+  } 
+ console.log("source node to update node");
+ console.log(source);
  update(source);
 }
 
 function click(d) {
   //updateBarChart(d.name);
-  console.log("[CollapsedTree click] selected_country, object sent to compute_data");
-  console.log(selected_country);
-  console.log({level: d.level, category: d.name, query: selected_country});
+  //console.log("[CollapsedTree click] selected_country, object sent to compute_data");
+  //console.log(selected_country);
+  //console.log({level: d.level, category: d.name, query: selected_country});
   compute_data({level: d.level, category: d.name, query: selected_country});
-  root.children.forEach(simulate_clicks)
-  
+  //root.children.forEach(simulate_clicks);
+  svg.selectAll("g.node").remove();
+ // var childsFile = root.children;  // Array of root childrens
+
+  // create a new queue
+var queue = new Queue();
+
+// enqueue an item
+queue.enqueue(root.children);
+
+// dequeue an item
+var item = queue.dequeue();
+
+while(item)
+{
+  for (i=0;i<item.length;i++)
+  {
+    if (item[i]._children.length!=0)
+    queue.enqueue(item[i]._children);
+    simulate_clicks(item[i]);
+  } 
+  item = queue.dequeue()
 }
+
+
+
+  /*var test = False;
+  var i = 0;
+  While (test)
+  {
+ //for (i = 0; i < childsFile.length; i++) 
+  //{
+    simulate_clicks(childsFile[i]);
+    // append children to next layer
+    console.log(childsFile[i]);
+    childsFile.push(childsFile[i]._children);
+    childsFile = childsFile.reduce(function(a, b) {return a.concat(b);}, []);
+    console.log(childsFile);
+    i++;
+  } */
+} 
 
